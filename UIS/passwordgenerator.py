b7e7aecf4c6fb8219572043796c9106e5d2f56fc
AyooN <axemi.magloire@gmail.com>
2021-12-19 20:16:08 -0300

    "on peux maintenant choisir la longueur du mot de passe

--- UIS/passwordgenerator.py
+++ UIS/passwordgenerator.py
@@ -2,14 +2,18 @@ import string
 from random import randint, choice
 from tkinter import *
 
+passwordmax = 20
+def changepasswordmaxlenght(n): 
+    global passwordmax 
+    passwordmax = n
+    print(n)
 
-def generatepassword(): 
-    passwordmin = 6
-    passwordmax = 12
+def generatepassword():
     all_chars = string.ascii_letters + string.digits
-    password = "".join(choice(all_chars) for x in range(randint(passwordmin,passwordmax)))
+    password = "".join(choice(all_chars) for x in range(0,int(passwordmax)))
     password_entry.delete(0, END)
     password_entry.insert(0, password)
+    print(passwordmax)
 
 
 colorbg = "#2D5C95"
@@ -22,7 +26,6 @@ root.config(bg=colorbg)
 frame = Frame(
     root,
     bg=colorbg
-
 )
 
 #création d'images
@@ -36,16 +39,16 @@ canvas = Canvas(
     bg=colorbg,
     bd=0,
     highlightthickness=0
-
 )
+
 canvas.create_image(width / 2 , height / 2, image=image)
 canvas.grid(row=0,column=0,sticky=W)
 #création d'une sous boite
 right_frame = Frame(
     frame,
     bg=colorbg
-
 )
+right_frame.grid(row=0,column=1,sticky=W)
 
 #création de titre
 labeltile = Label(
@@ -74,11 +77,26 @@ generatepasswordbutton = Button(
     command=generatepassword
 )
 generatepasswordbutton.pack(fill=X)
+scale_password_length = Scale(
+    right_frame,
+    orient='horizontal', 
+    from_=4, 
+    to=20,
+    resolution=2, 
+    tickinterval=16,
+    length=100, 
+    label="Password length", 
+    bg=colorbg,
+    fg="white",
+    highlightthickness=0, 
+    command=changepasswordmaxlenght
+)
 
+scale_password_length.pack(expand=True)
 
 #place la right_frame à droite de la frame principale
 
-right_frame.grid(row=0,column=1,sticky=W)
+
  
 #affiche la frame
 frame.pack(expand=YES)
